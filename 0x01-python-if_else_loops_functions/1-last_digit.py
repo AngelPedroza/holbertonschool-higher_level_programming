ae763c5602eea3d566d3851cac09bd32280c1c15
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-10 19:03:58 +0000

    [Add] Separate the string in 2 parts

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number < 0:
     module = number % -10
 else:
     module = number % 10
-print("Last digit of", number,"is", module, end=" ")
+print("Last digit of", number, "is", module, end=" ")
 if module > 5:
     print("and is greater than 5")
 elif module == 0:

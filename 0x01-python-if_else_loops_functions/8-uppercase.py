dd229bbe6c325caf5a49b07c6ead2137cade3bc4
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-10 21:49:29 +0000

    [Fix] Whitout ternary

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         y = str[i]
         if ord(str[i]) > 96 and ord(str[i]) < 123:
             y = chr(ord(str[i]) - 32)
-        print("{}".format(y), end='\n' if i == len(str) else '')
+        print("{}".format(y), end='')
     print()

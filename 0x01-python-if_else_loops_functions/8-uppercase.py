25192fdb9a51abd775941265a550025d88a266c0
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-10 21:56:51 +0000

    [Fix] env inside execve parameter

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3.4
 def uppercase(str):
     for i in range(len(str)):
         y = str[i]
         if ord(str[i]) > 96 and ord(str[i]) < 123:
             y = chr(ord(str[i]) - 32)
         print("{}".format(y), end='')
-    print("{}".format('\n'), end='')
+    print()

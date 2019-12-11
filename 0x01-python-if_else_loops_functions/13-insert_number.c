4f992eba9fa7d53676465047888b154ed580661c
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-11 00:54:53 +0000

    [Fix] If head are null

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,37 +9,41 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	listint_t *tmp;
-	listint_t *tmp2;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = number;
-	tmp = *head;
-	if (number < 0)
+	if (head && *head)
 	{
-		new->next = tmp;
-		*head = new;
-		return (new);
-	}
-	while (tmp->next != NULL)
-	{
-		if (tmp->n > number)
+		listint_t *new;
+		listint_t *tmp;
+		listint_t *tmp2;
+
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+
+		new->n = number;
+		tmp = *head;
+		if (number < 0)
 		{
-			tmp2->next = new;
 			new->next = tmp;
+			*head = new;
 			return (new);
 		}
-		tmp2 = tmp;
-		tmp = tmp->next;
-	}
-	if (tmp->next == NULL)
-	{
-		tmp->next = new;
-		new->next = NULL;
+		while (tmp->next != NULL)
+		{
+			if (tmp->n > number)
+			{
+				tmp2->next = new;
+				new->next = tmp;
+				return (new);
+			}
+			tmp2 = tmp;
+			tmp = tmp->next;
+		}
+		if (tmp->next == NULL)
+		{
+			tmp->next = new;
+			new->next = NULL;
+		}
+		return (NULL);
 	}
 	return (NULL);
 }

6ada7a29f55a934e01cc4005d04e7efdf53fb56e
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-12 19:53:34 +0000

    [Add] See how to use direcctories in python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3.4
-from calculator_1 import add, sub, mul, div
-import sys
+if __name__ == "__main__":
 
-if len(sys.argv) == 4:
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    sing = sys.argv[2]
+    if len(sys.argv) == 4:
 
-    optr = {'+': add,
-            '-': sub,
-            '*': mul,
-            '/': div}
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        sing = sys.argv[2]
 
-    for i in optr:
-        if sing == i:
-            res = optr[i](a, b)
-            print("{} {:} {} = {}".format(a, i, b, res))
-            exit(0)
+        optr = {'+': add,
+                '-': sub,
+                '*': mul,
+                '/': div}
 
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-else:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
+        for i in optr:
+            if sing == i:
+                res = optr[i](a, b)
+                print("{} {:} {} = {}".format(a, i, b, res))
+                exit(0)
+
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)

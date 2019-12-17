928a2e80f4435bf4bf3231063d9375f2be9890ef
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-17 19:14:43 +0000

    [Fix] Reduced and change the order

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/pythn3
 def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
-    lenght = len(my_list)
-    if idx < 0 or idx >= lenght:
-        return new_list
+    if idx < 0 or idx >= len(my_list):
+        return new_list[:]
 
+    new_list = my_list[:]
     new_list[idx] = element
     return new_list

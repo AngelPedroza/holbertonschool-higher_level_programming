f06cdbb54227fc8276b619139ae951e1bc500c6a
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-17 19:08:09 +0000

    [Fix] The idx >= lenght

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
-#!/usr/bin/pythn3.4
+#!/usr/bin/pythn3
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     lenght = len(my_list)
-    if idx < 0 or idx > lenght:
+    if idx < 0 or idx >= lenght:
         return new_list
 
     new_list[idx] = element

5da66542b340adc7447dac8cb2b9ee126866ca8f
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-17 16:43:09 +0000

    [Add] Create a copy with [:]

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pythn3.4
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    lenght = len(my_list)
+    if idx < 0 or idx > lenght:
+        return new_list
+
+    new_list[idx] = element
+    return new_list

98a8a70a49dc6c06927e21fa2556e2939901b13b
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-19 17:05:20 +0000

    [Fix] If I have more than 1 ocurrence

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    final = []
     for x in set_2:
         if x in set_1:
-            return x
+            final.append(x)
+    return final

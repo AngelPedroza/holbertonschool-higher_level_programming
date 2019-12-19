2c96110067e9c0e7862a37276ef08e4918776b86
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-19 19:21:20 +0000

    [Fix] More beaty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+
     count = 0
     for i in a_dictionary:
         if a_dictionary[i] is None:
@@ -11,9 +12,7 @@ def best_score(a_dictionary):
         if a_dictionary[i] > high:
             high = a_dictionary[i]
         count += 1
+
     for x in a_dictionary:
         if a_dictionary[x] == high:
-            break
-    if high == 0:
-        return None
-    return x
+            return x

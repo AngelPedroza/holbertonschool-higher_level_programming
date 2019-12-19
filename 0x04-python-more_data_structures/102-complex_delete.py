b62083a42b1173b49a7eb42de7f804140c2930a4
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-19 22:44:50 +0000

    [Fix] Make another list with the same size of the dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    bool = False
-    for i in a_dictionary:
-            if a_dictionary[i] == value:
-                bool = True
-                break
-    if bool is True:
-        dic = {}
-        for i in a_dictionary:
-            if a_dictionary[i] != value:
-                dic[i] = a_dictionary[i]
-        return dic
-    return a_dictionary
+        tam = list(a_dictionary.keys())
+        {a_dictionary.pop(i) for i in tam if a_dictionary[i] == value}
+        return a_dictionary

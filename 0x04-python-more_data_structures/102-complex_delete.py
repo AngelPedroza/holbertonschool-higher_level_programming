eb6959fae8ec997a3ed1551d55819ef778b9fb60
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-19 22:08:31 +0000

    [Fix] If not found the element

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    dic = {}
+    bool = False
     for i in a_dictionary:
-        if a_dictionary[i] != value:
-            dic[i] = a_dictionary[i]
-    return dic
+            if a_dictionary[i] == value:
+                bool = True
+                break
+    if bool is True:
+        dic = {}
+        for i in a_dictionary:
+            if a_dictionary[i] != value:
+                dic[i] = a_dictionary[i]
+        return dic
+    return a_dictionary

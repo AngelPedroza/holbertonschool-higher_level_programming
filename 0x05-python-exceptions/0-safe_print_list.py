e873b2e4d476c09d1ed457e3f71033317e10def8
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-08 21:37:46 +0000

    [Fix] Without return in except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[a]), end="")
             n += 1
         except:
-            print()
-            return n
+            break
     print()
     return n

caa3124b6c5077ac6fcd8978c2d096a026049836
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-08 19:17:36 +0000

    [Update] Change the orde of each statement

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/pyhton3
 def safe_print_division(a, b):
     try:
-        res = None
         res = a / b
-        return res
     except:
-        return None
+        res = None
     finally:
         print("Inside result: {}".format(res))
+        return res

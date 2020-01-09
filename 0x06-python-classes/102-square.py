265d8b7f48dc1e2e0e59fec000d41ce7cc042e3e
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-09 22:15:48 +0000

    [Add] Use the magic class for logic operators

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 class Square:
     pass
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -9,8 +10,23 @@ class Square:
         else:
             self.__size = size
 
-    def __eq__(self):
-        return repr(self.__size ** 2)
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
 
     def area(self):
         return (self.__size ** 2)

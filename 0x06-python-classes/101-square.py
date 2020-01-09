33599ae651b073169dacaa0803c4543e234ea2ea
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-09 21:23:43 +0000

    [Add] I can't with __str__ and __repr__ so I'd create a string with the same autput and return it

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+class Square:
+    pass
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+        if not isinstance(position[0], int) or not isinstance(position[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(position) > 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def __str__(self):
+        n_str = ""
+        if self.__size != 0:
+            if self.__position:
+                if self.__position[1] > 0:
+                    n_str += '\n'
+                for i in range(self.__size):
+                    n_str += str(' ' * self.__position[0])
+                    n_str += str('#' * self.__size + '\n')
+                n_str = n_str[:-1]
+            else:
+                for i in range(self.__size):
+                    n_str += str('#' * self.__size + '\n')
+                n_str = n_str[:-1]
+        return n_str
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        if self.__position[1] > 0:
+                for j in range(self.__position[1]):
+                    print()
+        for i in range(self.__size):
+            print(' ' * self.__position[0], end="")
+            print('#' * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not position[0] or not position[1] or position[2]:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(position[0], int) or not isinstance(position[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position

aceacd365a9fdf89ec9e195b37479d7d1c138f9d
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-10 00:59:08 +0000

    [Fix] If some position value is negative

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,12 +26,12 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if isinstance(value, int) and value >= 0:
-            self.__size = value
-        elif not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @property
     def position(self):
@@ -44,6 +44,8 @@ class Square:
         elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position

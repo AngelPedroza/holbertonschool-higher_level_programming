a46664b51ada2016f7db8c5e16b4cbc8e8455a61
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-13 00:13:52 +0000

    [Add] Simple cases

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_square(size):
+    if type(size) not in [int, float]:
+        raise TypeError("size must be an integer")
+    elif type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    elif type(size) is float:
+        size = int(size)
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for i in range(size):
+            print('#' * size)

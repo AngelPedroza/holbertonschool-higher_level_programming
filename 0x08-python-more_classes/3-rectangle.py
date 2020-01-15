7aace05dee639a2b73539e103e69e5546db8d704
Angel Pedroza <55103543+AngelPedroza@users.noreply.github.com>
2020-01-15 00:24:05 -0500

    [Fix] The variable height

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
         """Set a objet with width and height"""
 
         if height < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = height
 

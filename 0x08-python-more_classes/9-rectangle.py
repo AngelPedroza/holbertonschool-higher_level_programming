a490cec6ce294d04203082299b5951d0dd7fdbe3
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-15 05:39:24 +0000

    [Fix] If the zise is < 0

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -8,15 +8,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Set a objet with width and height"""
 
-        if height < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__height = height
-
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
+        self.__height = height
+        self.__width = width
 
         Rectangle.number_of_instances += 1
 
@@ -103,4 +96,6 @@ class Rectangle:
     # Class methods
     @classmethod
     def square(cls, size=0):
+        if size < 0:
+            return Rectangle(0, 0)
         return Rectangle(size, size)

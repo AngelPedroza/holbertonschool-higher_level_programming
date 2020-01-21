c96a7062b0da5a84bec09d8009619fd83f417f5e
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-21 15:37:16 +0000

    [Try] Second try

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton3
+class BaseGeometry:
+    """Know the area of a geometry shape"""
+
+    def area(self):
+        """A fake function"""
+        raise Exception("area() is not implemented")

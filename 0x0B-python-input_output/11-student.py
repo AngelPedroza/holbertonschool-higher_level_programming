482271ff8e9ba00e68c33e0bf0f20e4b847cc18c
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-21 23:06:16 +0000

    [Fix] With out import

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import json
-
-
 class Student:
     """The class for a student"""
     def __init__(self, first_name, last_name, age):
@@ -11,5 +8,4 @@ class Student:
 
     def to_json(self):
         """Retrieves a dictionary representation of a Student instance"""
-        json_format = json.dumps(self.__dict__)
-        return json.loads(json_format)
+        return self.__dict__

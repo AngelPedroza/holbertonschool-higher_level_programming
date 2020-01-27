edadfdc12eefe93fea665fe092aca8c39688a197
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-27 19:45:04 +0000

    [Fix] Use else

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,8 +20,8 @@ class Base:
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string"""

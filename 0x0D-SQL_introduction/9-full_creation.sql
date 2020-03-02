428cf0884bbae34f88cedf8c9435e021bbd4a898
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-02 14:23:16 -0500

    [Add] Put just one values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS
 second_table(id INT, name VARCHAR(256), score int);
 
-INSERT INTO second_table(id, name, score)
+INSERT INTO IF EXISTS second_table(id, name, score)
 VALUES
 	(1, 'Jhon', 10),
     	(2, 'Alex', 3),

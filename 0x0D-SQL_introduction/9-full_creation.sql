08185afb0efb660d22ba53e5c2bdd5291c3c1335
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-02 14:27:49 -0500

    [Add] Put just one values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,13 @@
 -- create a second table for hbtn_0c_0
-CREATE TABLE IF NOT EXISTS
-second_table(id INT, name VARCHAR(256), score int);
+CREATE TABLE IF NOT EXISTS second_table (
+       id INT,
+       name VARCHAR(256),
+       score int
+);
 
-INSERT INTO IF EXISTS second_table(id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES
-	(1, 'Jhon', 10),
+	(1, 'John', 10),
     	(2, 'Alex', 3),
 	(3, 'Bob', 14),
 	(4, 'George', 8);

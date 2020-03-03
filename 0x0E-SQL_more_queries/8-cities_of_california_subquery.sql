4a191dbf9c078d3753fd7e5493a3fbb004f92e02
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-03 14:41:23 -0500

    [Add] name = california not id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 -- just do it
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.state_id=states.id AND states.id="California"
+WHERE cities.state_id=states.id AND states.name="California"
 ORDER BY cities.id ASC;

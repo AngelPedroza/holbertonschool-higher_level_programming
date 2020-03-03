7a39a988456d944fe92748962e71216ba4704c70
Derhks <derhks@gmail.com>
2020-03-03 12:37:47 -0500

    [Add] Add use

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- Another try
 -- Add another comment  to prouve
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-       id INT UNIQUE AUTO_INCREMENT NOT NULL DEFAULT 1,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY (id),

4591483d41ca356985cedf4b9d0010de60d4285b
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-03 18:06:34 -0500

    [Add] Make two querys

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,15 @@
 -- Just one select and could use Join
 -- Just do it
 SELECT tv_genres.name
-FROM tv_genres, tv_show_genres
-WHERE tv_genres.id=tv_show_genres.genre_id AND tv_show_genres.show_id=(
-      SELECT tv_shows.id
-      FROM tv_shows
-      WHERE tv_shows.title="Dexter")
--- tv_show_genres.genre_id=tv_genres.id)
-ORDER BY tv_genres.name ASC;
+FROM tv_genres
+LEFT JOIN (
+     SELECT tv_genres.id, tv_genres.name
+     FROM tv_genres
+     JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+     JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+     WHERE tv_shows.title="Dexter"
+     ORDER BY tv_genres.name ASC
+) AS gen_dex
+ON tv_genres.id=gen_dex.id
+WHERE gen_dex.id IS NULL
+ORDER BY tv_genres.name;

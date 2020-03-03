f7ea1df81d7a7c53682f3fbf22898de5ca62017e
Derhks <derhks@gmail.com>
2020-03-03 12:29:03 -0500

    [Add] DEFAULT

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- Add another comment  to prouve
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-       id INT UNIQUE AUTO_INCREMENT NOT NULL,
+       id INT UNIQUE AUTO_INCREMENT NOT NULL DEFAULT 1,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY (id),

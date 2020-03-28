7bd4098ba3e92bd2d5adda062f93a3fa3beab315
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-28 17:06:28 -0500

    [Add] add the if

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,38 +3,38 @@
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[2],
+                         passwd=sys.argv[1],
+                         db=sys.argv[3])
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=sys.argv[2],
-                     passwd=sys.argv[1],
-                     db=sys.argv[3])
+    cur = db.cursor()
 
-cur = db.cursor()
+    # Execute the query
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON states.id = cities.state_id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC;
+    """
+    cur.execute(query, (sys.argv[4], ))
 
-# Execute the query
-query = """
-SELECT cities.name
-FROM cities
-JOIN states ON states.id = cities.state_id
-WHERE states.name = %s
-ORDER BY cities.id ASC;
-"""
-cur.execute(query, (sys.argv[4], ))
+    my_list = []
+    str1 = ""
+    for row in cur.fetchall():
+        my_list.append(row[0])
 
-my_list = []
-str1 = ""
-for row in cur.fetchall():
-    my_list.append(row[0])
+    for i in range(len(my_list)):
+        if i == len(my_list) - 1:
+            str1 += my_list[i]
+        else:
+            str1 += "{}, ".format(my_list[i])
 
-for i in range(len(my_list)):
-    if i == len(my_list) - 1:
-        str1 += my_list[i]
-    else:
-        str1 += "{}, ".format(my_list[i])
+    print(str1)
 
-print(str1)
-
-# Close the connection
-cur.close()
-db.close()
+    # Close the connection
+    cur.close()
+    db.close()

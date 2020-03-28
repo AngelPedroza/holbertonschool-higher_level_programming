c15922bd0d529762abb4761df9b90003eb56e4a4
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-28 16:59:54 -0500

    [Add] add the if

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,22 +3,21 @@
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[2],
+                         passwd=sys.argv[1],
+                         db=sys.argv[3])
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=sys.argv[2],
-                     passwd=sys.argv[1],
-                     db=sys.argv[3])
+    cur = db.cursor()
 
-cur = db.cursor()
+    # Execute the query
+    query = "SELECT id, name FROM states WHERE name='{}' ORDER BY id ASC;".format(
+        sys.argv[4])
+    cur.execute(query)
+    for row in cur.fetchall():
+        print(row)
 
-# Execute the query
-query = "SELECT id, name FROM states WHERE name='{}' ORDER BY id ASC;".format(
-    sys.argv[4])
-cur.execute(query)
-
-for row in cur.fetchall():
-    print(row)
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()

3f3e7a972de0173903a2ba92547bcf6b30414e63
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-28 16:50:49 -0500

    [Add] the if ==

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,21 +3,21 @@
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[2],
+                         passwd=sys.argv[1],
+                         db=sys.argv[3])
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=sys.argv[2],
-                     passwd=sys.argv[1],
-                     db=sys.argv[3])
+    cur = db.cursor()
 
-cur = db.cursor()
+    # Execute the query
+    cur.execute('SELECT id, name FROM states\
+    WHERE name LIKE "N%" ORDER BY id ASC;')
 
-# Execute the query
-cur.execute('SELECT id, name FROM states\
-             WHERE name LIKE "N%" ORDER BY id ASC;')
+    for row in cur.fetchall():
+        print(row)
 
-for row in cur.fetchall():
-    print(row)
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()

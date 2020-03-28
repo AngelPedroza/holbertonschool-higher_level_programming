108b35a1df160349c5ed70bb5fb069245a24fdad
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-28 17:18:47 -0500

    [Fix] Delete a blancspace

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -9,14 +9,12 @@ if __name__ == '__main__':
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
 
-
     sql_username = sys.argv[1]
     sql_pwd = sys.argv[2]
     db_name = sys.argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sql_username, sql_pwd, db_name), pool_pre_ping=True)
-
 
     Session = sessionmaker(bind=engine)
     session = Session()

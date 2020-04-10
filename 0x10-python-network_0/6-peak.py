ee4deebb75e2da6d001742c6910ec5d4679e9b18
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-10 10:54:04 -0500

    [Fix] Delete conditionals

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,40 +5,32 @@ def find_peak(list_of_integers):
     """Funtion to find the peak"""
     l = list_of_integers
 
-    if l is None:
+    if len(l) == 0:
         return None
-    if len(l) == 1:
-        return l[0]
-    if len(l) == 2:
-        if l[0] <= l[1]:
-            return l[1]
-        else:
-            return l[0]
 
-    if len(l) >= 3:
-        res = 0
-        r_left = 0
-        r_right = 0
-        flag = 0
-        for n in range(len(l) - 1):
-            if l[n - 1] <= l[n] and l[n + 1] <= l[n] and flag == 0:
-                res = l[n]
+    res = 0
+    r_left = 0
+    r_right = 0
+    flag = 0
+    for n in range(len(l) - 1):
+        if l[n - 1] <= l[n] and l[n + 1] <= l[n] and flag == 0:
+            res = l[n]
 
-                if r_left < 0:
-                    r_left *= -1
-                if r_right < 0:
-                    r_right *= -1
+            if r_left < 0:
+                r_left *= -1
+            if r_right < 0:
+                r_right *= -1
 
-                if r_left < l[n] - l[n - 1]:
-                    r_left = l[n] - l[n - 1]
-                    flag = 1
-                else:
-                    flag = 0
+            if r_left < l[n] - l[n - 1]:
+                r_left = l[n] - l[n - 1]
+                flag = 1
+            else:
+                flag = 0
 
-                if r_right < l[n] - l[n + 1]:
-                    r_right = l[n] - l[n + 1]
-                    flag = 1
-                else:
-                    flag = 0
+            if r_right < l[n] - l[n + 1]:
+                r_right = l[n] - l[n + 1]
+                flag = 1
+            else:
+                flag = 0
 
-        return res
+    return res

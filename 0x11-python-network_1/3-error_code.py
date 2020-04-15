25b70c968d7b99bd81e724e3256db560446ab1d0
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-15 12:22:35 -0500

    [Fix] Pep8 style and a test delete

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,8 +6,7 @@ if __name__ == "__main__":
 
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
-            li = str(response.read())
+            li = str(response.read().decode('utf-8'))
             print(li)
-            print(response.read())
     except urllib.error.HTTPError as error:
             print("Error code: {}".format(error.code))

dd981ac815bf44fabc20311d4bec9d94ebb19e2e
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-05 13:12:30 -0500

    [Fix] If pass more than 1 argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2]) { console.log('Argument found'); } else { console.log('No argument'); }
+if (!process.argv[2]) {
+  console.log('No argument');
+} else if (process.argv[2] && !process.argv[3]) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}

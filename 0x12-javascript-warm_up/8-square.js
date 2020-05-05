247ce3b9ddd9df927e67fa7c62b2f79500abb6a0
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-05 13:14:49 -0500

    [Fix] Write well the X not x

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myVar = process.argv[2];
 if (process.argv[2] && parseInt(myVar, 10)) {
-  const array = 'x';
+  const array = 'X';
   const intiger = parseInt(myVar, 10);
   for (let i = 0; i < intiger; i++) {
     for (let j = 0; j < intiger; j++) {

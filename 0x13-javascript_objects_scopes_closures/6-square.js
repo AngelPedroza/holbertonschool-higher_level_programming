fba994cd9a8abd3cab22ad64dc89e6e39ff40f92
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-11 13:33:34 -0500

    [Add] Call the before Square class with a require an use it witg another name

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Square2 = require('./5-square');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) { process.stdout.write('X'); }
-      console.log('');
-    }
-  }
-
-  rotate () {
-    const a = this.width;
-    const b = this.height;
-    this.width = b;
-    this.height = a;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends Square2 {
   constructor (size) {
-    super(size, size);
+    super(size);
   }
 
   charPrint (c) {
@@ -41,5 +15,4 @@ class Square extends Rectangle {
   }
 }
 
-module.exports = Rectangle;
 module.exports = Square;

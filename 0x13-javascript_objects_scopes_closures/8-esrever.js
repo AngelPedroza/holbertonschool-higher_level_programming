3ccdb4cd7e4f09670c7863bb4db6ae12215eeba8
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-11 13:56:44 -0500

    [Add] Put the last element in the begin of a new list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let fin = list.length - 1;
-  let tmp = '';
-  for (let i = 0; i < list.length; i++) {
-    if (i >= fin) { return list; }
-
-    tmp = list[i];
-    list[i] = list[fin];
-    list[fin] = tmp;
-
-    fin -= 1;
+  const newList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
   }
+  return newList;
 };

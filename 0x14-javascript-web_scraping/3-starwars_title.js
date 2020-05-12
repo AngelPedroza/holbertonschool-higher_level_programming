852b71806df202cbe5772f3f5838ff572ca79ec4
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-12 13:30:03 -0500

    [Add] fix bad if

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-if (0 < process.argv[2] && process.argv[2] < 7) {
-  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-  request.get(url, function (err, response, body) {
-    if (err) { console.log(err); } else {
-      console.log(JSON.parse(body).title);
-    }
-  });
-}
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(url, function (err, response, body) {
+  if (err) { console.log(err); } else {
+    console.log(JSON.parse(body).title);
+  }
+});

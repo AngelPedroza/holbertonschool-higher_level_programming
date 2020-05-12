4f1509f8fcb7cd5d26b69bb2f4a24abe64e82d40
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-12 13:09:08 -0500

    [Add] Without error manage

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get(url, function (err, response, body) {
-  if (err) { console.log(err); } else {
+  if (err == null){
     console.log(JSON.parse(body).title);
   }
 });

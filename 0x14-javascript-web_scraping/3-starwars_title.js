eaf22105243d6d8ba150c77937e3fa07922f87d0
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-12 13:16:00 -0500

    [Add] Accept an specific range of ids

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(url, function (err, response, body) {
-  if (err) { console.log(err); } else {
-    console.log(JSON.parse(body).title);
-  }
-});
+if (0 < process.argv[2] && process.argv[2] < 7) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+  request.get(url, function (err, response, body) {
+    if (err) { console.log(err); } else {
+      console.log(JSON.parse(body).title);
+    }
+  });
+}

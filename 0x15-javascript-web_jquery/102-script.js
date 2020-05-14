5a80b8c7a81a132fd963d73b1665cad43cd19bec
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-14 16:33:00 -0500

    [Fix] The style

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,12 +1,10 @@
-window.onload = function() {
-  $("INPUT#btn_translate").click(function() {
-    const leng = $("INPUT#language_code").val();
+window.onload = function () {
+  $('INPUT#btn_translate').click(function () {
+    const leng = $('INPUT#language_code').val();
     const url = 'https://fourtonfish.com/hellosalut/?lang=' + leng;
     console.log(url);
-    fetch(url).then(function(response) {
-      return response.json();
-    }).then(function(myJson) {
-      $("DIV#hello").text(myJson["hello"]);
+    $.get(url, function (data) {
+      $('DIV#hello').text(data.hello);
     });
   });
-}
+};
--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,26 +1,20 @@
-window.onload = function() {
-  $("INPUT#btn_translate").click(function() {
-    const leng = $("INPUT#language_code").val();
+window.onload = function () {
+  $('INPUT#btn_translate').click(function () {
+    const leng = $('INPUT#language_code').val();
     const url = 'https://fourtonfish.com/hellosalut/?lang=' + leng;
-    console.log(url);
-    fetch(url).then(function(response) {
-      return response.json();
-    }).then(function(myJson) {
-      $("DIV#hello").text(myJson["hello"]);
+    $.get(url, function (data) {
+      $('DIV#hello').text(data.hello);
     });
   });
 
-  $("INPUT#language_code").keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
-      const leng = $("INPUT#language_code").val();
+  $('INPUT#language_code').keypress(function (event) {
+    const keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode === '13') {
+      const leng = $('INPUT#language_code').val();
       const url = 'https://fourtonfish.com/hellosalut/?lang=' + leng;
-      console.log(url);
-      fetch(url).then(function(response) {
-	return response.json();
-      }).then(function(myJson) {
-	$("DIV#hello").text(myJson["hello"]);
+      $.get(url, function (data) {
+        $('DIV#hello').text(data.hello);
       });
     }
   });
-}
+};
